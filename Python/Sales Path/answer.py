7c7e533a4f78949ce58a4eb35b14f94bd7fd31b8
vulkrim <vulkrim@gmail.com>
2018-09-18 16:29:30 -0700

    added answers to answer key and modified code in sales path

--- Python/Sales Path/answer.py
+++ Python/Sales Path/answer.py
@@ -4,4 +4,11 @@ def get_cheapest_cost(rootNode, cost=0):
   if not rootNode.children:
     return rootNode.cost + cost
   return min([get_cheapest_cost(child, cost + rootNode.cost) for child in rootNode.children])
+
+class Node:
+
+    def __init__(self, cost, parent=None):
+    self.cost = cost
+    self.children = []
+    self.parent = None
   
