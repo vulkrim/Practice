aedd7f31a1f963f3bc47c11f10fb34d7f4087771
vulkrim <vulkrim@gmail.com>
2018-09-20 08:23:49 -0700

    finished answerKeys building main

--- Python/completed.py
+++ Python/completed.py
@@ -1,4 +1,4 @@
-completed = {
+complete = {
         'Award Budget Cuts':'find_grants_cap',
         'Flatten a Dictionary':'flatten_dictionary',
         'Number of Paths':'num_of_paths_to',
---
+++ Python/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from completed import complete
+
+for i in complete:
+    f
+
+if __name__ == '__main__':
+
+
